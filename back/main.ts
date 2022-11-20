9730c4bf07ff3783935616f8ec7db363ca2bbdeb
Dann Luciano <dannluciano@gmail.com>
2022-11-20 01:55:25 -0300

    Add Postgres

--- back/main.ts
+++ back/main.ts
@@ -1,8 +1,25 @@
 import { serve } from "./deps.ts";
+import { PGClient } from "./deps.ts";
+
+const dbConfig = {
+  applicationName: "learn-docker",
+  hostname: Deno.env.get("POSTGRES_HOST"),
+  database: Deno.env.get("POSTGRES_DB"),
+  user: Deno.env.get("POSTGRES_USER"),
+  password: Deno.env.get("POSTGRES_PASSWORD"),
+  port: 5432,
+}
+const db = new PGClient(dbConfig);  
+
+try {
+  await db.connect();
+} catch (error) {
+  console.error(error)
+}
 
 const port = 80;
 
-const handler = (request: Request): Response => {
+const handler = async (request: Request): Promise<Response> => {
   console.info(request.url)
   const path = request.url.replace('http://localhost', '')
   
@@ -11,8 +28,11 @@ const handler = (request: Request): Response => {
     return new Response(JSON.stringify(json), { status: 200, headers: {'content-type': 'application/json' }});
   }
 
+  const result = await db.queryArray("SELECT NOW()");
+
   const obj = {
-    msg: "Hello World"
+    msg: "Hello World",
+    now: result.rows[0]
   }
   return new Response(JSON.stringify(obj), { status: 200, headers: {'content-type': 'application/json' }});
 };
