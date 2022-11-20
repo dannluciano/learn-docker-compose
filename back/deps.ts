ef9fea2980deedf6871a099786359a8258630fd9
Dann Luciano <dannluciano@gmail.com>
2022-11-20 03:16:32 -0300

    Add Prometheus

--- back/deps.ts
+++ back/deps.ts
@@ -1,2 +1,4 @@
 export { serve } from "https://deno.land/std@0.165.0/http/server.ts";
 export { Client as PGClient } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+export { Counter as PrometheusCounter } from "https://deno.land/x/ts_prometheus/mod.ts";
+export { Registry as PrometheusRegistry } from "https://deno.land/x/ts_prometheus/mod.ts";
--- back/main.ts
+++ back/main.ts
@@ -1,5 +1,12 @@
 import { serve } from "./deps.ts";
 import { PGClient } from "./deps.ts";
+import { PrometheusCounter, PrometheusRegistry } from "./deps.ts";
+
+const counter = PrometheusCounter.with({
+  name: "http_requests_total",
+  help: "The total HTTP requests",
+  labels: ["path", "method", "status"],
+});
 
 const dbConfig = {
   applicationName: "learn-docker",
@@ -13,6 +20,7 @@ const db = new PGClient(dbConfig);
 
 try {
   await db.connect();
+  console.info("Database Connected!")
 } catch (error) {
   console.error(error)
 }
@@ -21,11 +29,18 @@ const port = 80;
 
 const handler = async (request: Request): Promise<Response> => {
   console.info(request.url)
-  const path = request.url.replace('http://localhost', '')
+  const url = new URL(request.url);
+  const path = url.pathname
+  const method = request.method
   
   if (path.startsWith('/api/health')) {
     const json = {status: 'OK'}
     return new Response(JSON.stringify(json), { status: 200, headers: {'content-type': 'application/json' }});
+  }
+
+  if (method === 'GET' && path.startsWith('/api/metrics')) {
+    const body = PrometheusRegistry.default.metrics();
+    return new Response(body, { status: 200, headers: {'content-type': '' }});
   }
 
   const result = await db.queryArray("SELECT NOW()");
@@ -34,7 +49,16 @@ const handler = async (request: Request): Promise<Response> => {
     msg: "Hello World",
     now: result.rows[0]
   }
-  return new Response(JSON.stringify(obj), { status: 200, headers: {'content-type': 'application/json' }});
+  const status = 200
+  counter.labels({
+    path,
+    method,
+    status
+  }).inc();
+  console.info(path, method, status)
+  return new Response(
+    JSON.stringify(obj), { status, headers: {'content-type': 'application/json' }}
+  );
 };
 
 console.log(`HTTP webserver running. Access it at: http://localhost:80/`);
